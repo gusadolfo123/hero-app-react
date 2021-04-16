3719608c911e40b79a189ef40fb4692329a458d9
gusadolfo123 <gusadolfo123@hotmail.com>
2021-04-16 18:54:12 -0500

    finish initial app

--- src/components/search/SearchPage.jsx
+++ src/components/search/SearchPage.jsx
@@ -1,26 +1,33 @@
-import React, { useState } from "react";
-import { heroes } from "../../data/heroes";
+import React, { useMemo, useState } from "react";
+import { useLocation } from "react-router";
+// import { heroes } from "../../data/heroes";
 import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../ui/heroes/HeroCard";
+import querystring from "query-string";
+import { getHeroesByName } from "../../selectors/getHeroByName";
 
-export const SearchPage = () => {
-  const [heroesFilter, setHeroes] = useState([]);
+export const SearchPage = ({ history }) => {
+  const location = useLocation();
+  const { q = "" } = querystring.parse(location.search);
   const [formValues, handlerInputChange, reset] = useForm({
-    search: "",
+    search: q,
   });
 
   const { search } = formValues;
+  //   const [heroesFilter, setHeroes] = useState(getHeroesByName(search));
+  const heroesFilter = useMemo(() => getHeroesByName(q), [q]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (search.length >= 3) {
-      const data = heroes.filter((hero) =>
-        //   hero.superhero.trim().toLowerCase().startsWith(search.toLowerCase())
-        hero.superhero.toLowerCase().includes(search.toLowerCase())
-      );
-      setHeroes(data);
-      reset();
-    }
+    history.push(`?q=${search}`);
+    // if (search.length >= 3) {
+    //   const data = heroes.filter((hero) =>
+    //     //   hero.superhero.trim().toLowerCase().startsWith(search.toLowerCase())
+    //     hero.superhero.toLowerCase().includes(search.toLowerCase())
+    //   );
+    //   setHeroes(data);
+    //   reset();
+    // }
   };
 
   return (
@@ -49,6 +56,13 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
+
+          {q === "" && <div className="alert alert-info">Search Hero.</div>}
+
+          {q !== "" && heroesFilter.length === 0 && (
+            <div className="alert alert-danger">No Hero Found With {q}</div>
+          )}
+
           {heroesFilter.map((hero) => (
             <HeroCard key={hero.id} {...hero} />
           ))}
